4911d90918e8d48f6b43a4c7f7639d89709e8593
Arianna Pickard <ariannajpickard@gmail.com>
2020-09-09 12:42:15 -0500

    adding testing outline

---
+++ app/src/test/java/com/example/LifestyleApp/UserInfo2Tests.java
@@ -0,0 +1,22 @@
+package com.example.LifestyleApp;
+
+import org.junit.Test;
+
+class userInfo2InputTests {
+
+    @Test
+    public void testCountryInput() {
+
+    }
+
+    @Test
+    public void testCityInput() {
+
+    }
+
+    @Test
+    public void testWhoCanSee() {
+
+    }
+
+}
---
+++ app/src/test/java/com/example/LifestyleApp/userInfo3InputTests.java
@@ -0,0 +1,12 @@
+package com.example.LifestyleApp;
+
+import org.junit.Test;
+
+class selfieTests {
+
+    @Test
+    public void testSnapSelfie() {
+
+    }
+
+}
