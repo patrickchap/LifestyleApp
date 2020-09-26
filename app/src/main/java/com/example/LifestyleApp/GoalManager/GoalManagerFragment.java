16ad36065e2f660fdc4e087bfddc7ecd88cadd25
Patrick <patrick.chapple28@icloud.com>
2020-09-25 23:24:50 -0600

    update formating for goal manager

--- app/src/main/java/com/example/LifestyleApp/GoalManager/GoalManagerFragment.java
+++ app/src/main/java/com/example/LifestyleApp/GoalManager/GoalManagerFragment.java
@@ -9,9 +9,13 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import com.example.LifestyleApp.R;
 import com.example.LifestyleApp.UserInfo.User;
+
+import Dialogs.HeightPickerDialog;
+import Dialogs.WeightPickerDialog;
 
 public class GoalManagerFragment extends Fragment implements View.OnClickListener {
     private SeekBar seekBar;
@@ -42,10 +46,16 @@ public class GoalManagerFragment extends Fragment implements View.OnClickListene
         mLose = view.findViewById(R.id.loseBtn);
         mMaintain = view.findViewById(R.id.maintainBtn);
         mGain = view.findViewById(R.id.gainBtn);
+        mGoalWeight = view.findViewById(R.id.goalWeightTextView);
+        mActivity = view.findViewById(R.id.activityLevelTextView);
 
-        mWeight.setText(String.format("%f", user.getWeight()));
-        mHeight.setText("" + user.height);
+        mWeight.setText(user.getWeight() + " >");
+        mHeight.setText(user.height + " >");
 
+        mWeight.setOnClickListener(this);
+        mHeight.setOnClickListener(this);
+        mGoalWeight.setOnClickListener(this);
+        mActivity.setOnClickListener(this);
         mLose.setOnClickListener(this);
         mMaintain.setOnClickListener(this);
         mGain.setOnClickListener(this);
@@ -81,7 +91,6 @@ public class GoalManagerFragment extends Fragment implements View.OnClickListene
                 user.setGoal("lose");
                 break;
             }
-
             case R.id.maintainBtn: {
                 mLose.setBackgroundColor(getResources().getColor(R.color.button_gray));
                 mMaintain.setBackgroundColor(getResources().getColor(R.color.continue_blue));
@@ -89,7 +98,6 @@ public class GoalManagerFragment extends Fragment implements View.OnClickListene
                 user.setGoal("maintain");
                 break;
             }
-
             case R.id.gainBtn: {
                 mLose.setBackgroundColor(getResources().getColor(R.color.button_gray));
                 mMaintain.setBackgroundColor(getResources().getColor(R.color.button_gray));
@@ -97,6 +105,26 @@ public class GoalManagerFragment extends Fragment implements View.OnClickListene
                 user.setGoal("gain");
                 break;
             }
+            case R.id.weightTextView: {
+                DialogFragment dialogFragment = new WeightPickerDialog();
+                assert getFragmentManager() != null;
+                dialogFragment.show(getFragmentManager(), "WeightPicker");
+                break;
+            }
+            case R.id.heightTextView: {
+                DialogFragment dialogFragment = new HeightPickerDialog();
+                dialogFragment.show(getFragmentManager(), "HeightPicker");
+                break;
+            }
+            case R.id.goalWeightTextView: {
+                //goalweight dialog
+                break;
+            }
+            case R.id.activityLevelTextView: {
+                //activity level dialog box
+                break;
+            }
+
         }
     }
 }
