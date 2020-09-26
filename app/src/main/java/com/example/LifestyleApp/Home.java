dad7d9a2b6ff189bbf1116b44c52aeba32d866a8
Patrick <patrick.chapple28@icloud.com>
2020-09-25 20:33:54 -0600

    add measurements to home screen

--- app/src/main/java/com/example/LifestyleApp/Home.java
+++ app/src/main/java/com/example/LifestyleApp/Home.java
@@ -1,9 +1,13 @@
 package com.example.LifestyleApp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +19,14 @@ import com.example.LifestyleApp.UserInfo.User;
 public class Home extends AppCompatActivity implements View.OnClickListener{
     Button moduleBtn;
     User user;
+    ImageView mUserProfilePicture;
+    TextView mBMI;
+    TextView mWeight;
+    TextView mHeight;
+    TextView mGoalWeight;
+    TextView mBMR;
+    TextView mActivityLevel;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,6 +36,26 @@ public class Home extends AppCompatActivity implements View.OnClickListener{
 
         moduleBtn = findViewById(R.id.moduleButton);
         moduleBtn.setOnClickListener(this);
+
+        mBMI = findViewById(R.id.bmiValue);
+        mBMI.setText(user.getBmi() + "");
+
+        mWeight = findViewById(R.id.weightValue);
+        mWeight.setText(user.getWeight() + "");
+
+        mHeight = findViewById(R.id.heightValue);
+        mHeight.setText(user.getHeight() + "");
+
+        mGoalWeight = findViewById(R.id.goalWeightValue);
+        mBMR = findViewById(R.id.bmrValue);
+        mActivityLevel = findViewById(R.id.activityLevelValue);
+
+
+
+        mUserProfilePicture = findViewById(R.id.profilePictureIV);
+        byte[] byteArray = user.getProfilePicture();//intent.getByteArrayExtra("profilePicture");
+        Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        mUserProfilePicture.setImageBitmap(bmp);
 
     }
 
